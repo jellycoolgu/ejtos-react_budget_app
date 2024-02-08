b339d4f02f0205ed85f8f5fa9a525f1d27c013b6
Jellycool Gu <jellycoolgu@gmail.com>
2024-02-08 13:43:26 -0800

    update inclease, declease,delete icon by JC

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,10 @@
 
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+//import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import logoPlug from '../plus-4-256.ico';
+import logoMinus from '../minus.jpg';
+import logoXmark from '../x-mark-3-xxl.png';
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -43,9 +46,9 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td><img src={logoPlug} alt='+' style={{ width: "30px", height: "30px" }} onClick={event=> increaseAllocation(props.name)} /></td>
+        <td><img src={logoMinus} alt='-' style={{ width: "30px", height: "30px" }} onClick={event=> decreaseAllocation(props.name)} /></td>
+        <td><img src={logoXmark} alt='X' style={{ width: "30px", height: "30px" }} onClick={handleDeleteExpense} /></td>
         </tr>
     );
 };
